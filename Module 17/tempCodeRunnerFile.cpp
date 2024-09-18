17e0f17bfe62d4ff29808da243925e26a81de395
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2024-09-18 23:23:49 +0600

    tree order explore like pre_order, post_order, and in_order

---
+++ Module 17/tempCodeRunnerFile.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *left, *right;
+    Node(int value)
+    {
+        this->value = value;
+        this->right = NULL;
+        this->left = NULL;
+    }
+};
+
+void reverse(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->value << " ";
+    reverse(root->left);
+    reverse(root->right);
+};
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    b->right = e;
+    c->left = h;
+    d->left = i;
+    e->left = f;
+    e->right = g;
+
+    reverse(root);
+    return 0;
+}
